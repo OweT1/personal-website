563cddb63abfc6c95a034ce3cf10d045a3b91acf
OweT1 <owentan@hotmail.sg>
2026-08-09 20:22:07 +0800

    feat: minor edits for better user experience

--- src/components/generalComponents/navbar.tsx
+++ src/components/generalComponents/navbar.tsx
@@ -28,7 +28,7 @@ export function NavBar() {
           onClick={() => navigate("/")}
           className="font-bold text-slate-900 tracking-tight hover:text-red-600 transition-colors cursor-pointer"
         >
-          Owen Tan
+          Owen
         </button>
 
         <div className="flex items-center gap-6">
